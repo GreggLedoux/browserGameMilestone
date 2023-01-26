e77efaa2b555ec6890c3d6557f93e5e8d8a0c39d
GreggLedoux <guitlouie@gmail.com>
2023-01-26 18:19:22 -0500

    begining JS.  Need more research

---
+++ assets/index.js
@@ -0,0 +1,9 @@
+const selectors = {
+    boardContainer: document.querySelector('.boardcontainer'),
+    board: document.querySelevtor('.board'),
+    moves: document.querySelector('.moves'),
+    timer: document.querySelector('.timer'),
+    start: document.querySelector('button'),
+    win: document.querySelector('win')
+}
+
