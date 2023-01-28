4f3064f509b47c9909c68cd9ee68cfd0322f3306
GreggLedoux <guitlouie@gmail.com>
2023-01-28 10:12:27 -0500

    added shuffle array functions

--- assets/index.js
+++ assets/index.js
@@ -7,6 +7,45 @@ const selectors = {
     win: document.querySelector('win')
 }
 
+const state = {
+    gameStarted: false,
+    flippedCards: 0,
+    totalFlips: 0,
+    totalTime: 0,
+    loop: null
+}
+
+const shuffle = array => {
+    const clonedArray = [...array]
+
+    for (let index = clonedArray.length - 1; index > 0; index--) {
+        const randomIndex = Math.floor(Math.random() * (index +1))
+        const original = clonedArray[randomIndex]
+
+        clonedArray[index] = clonedArray[randomIndex]
+        clonedArray[randomIndex] = original
+    }
+
+    return clonedArray
+}
+
+const pickRandom = (array, items) => {
+    const clonedArray = [...array]
+    const randomPicks = []
+
+    for (let index = 0; index < items; index++) {
+        const randomIndex = Math.floor(Math.random() * clonedArray.length)
+
+        randomPicks.push(clonedArray[randomIndex])
+        clonedArray.splice(randomIndex, 1)
+    }
+
+    return randomPicks
+}
+
+
+
+
 const attachEventListeners = ()=> {
     document.addEventListener('click', event => {
         const eventTarget = event.target
@@ -22,3 +61,4 @@ const attachEventListeners = ()=> {
 
 generateGame()
 attachEventListeners()
+
