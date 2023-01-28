8b1ec5bc994fc6584a3a32d85e4d5c8d742dd9e2
GreggLedoux <guitlouie@gmail.com>
2023-01-28 11:44:09 -0500

    game start loop added again

--- assets/index.js
+++ assets/index.js
@@ -69,6 +69,19 @@ const parser = new DOMParser().parseFromString(cards, 'text/html')
 selectors.board.replaceWith(parser.querySelector('.board'))
 }
 
+const startGame = () => {
+    state.gameStarted = true
+    selectors.start.classList.add('disabled')
+
+    state.loop = setInterval(() => {
+        state.totalTime++
+
+        selectors.moves.innerText = `${state.totalFlips} moves`
+        selectors.timer.innerText = `time: ${state.totalTime} sec`
+    }, 1000)
+}
+
+
 const attachEventListeners = ()=> {
     document.addEventListener('click', event => {
         const eventTarget = event.target
